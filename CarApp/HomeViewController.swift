57a5a71ce7d6ed4fbc2b43f703ce62f69b4ac7e8
urbanintell <luiikromah@gmail.com>
2017-10-14 12:23:24 -0400

    COREML vehicles integration

--- CarApp/HomeViewController.swift
+++ CarApp/HomeViewController.swift
@@ -11,65 +11,56 @@ import SceneKit
 import ARKit
 import Vision
 import MapKit
-import CocoaLumberjack
 
-class HomeViewController: UIViewController, ARSCNViewDelegate,  MKMapViewDelegate, SceneLocationViewDelegate {
-    
+
+class HomeViewController: UIViewController, ARSCNViewDelegate {
+
     // SCENE
     @IBOutlet var sceneView: ARSCNView!
     let bubbleDepth : Float = 0.01 // the 'depth' of 3D text
     var latestPrediction : String = "…" // a variable containing the latest CoreML prediction
-    
+
     // COREML
     var visionRequests = [VNRequest]()
     let dispatchQueueML = DispatchQueue(label: "com.hw.dispatchqueueml") // A Serial Queue
     @IBOutlet weak var debugTextView: UITextView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
-        
+
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
+
         // Create a new scene
         let scene = SCNScene()
-        
+
         // Set the scene to the view
         sceneView.scene = scene
-        
+
         // Enable Default Lighting - makes the 3D text a bit poppier.
         sceneView.autoenablesDefaultLighting = true
-        
+
         //////////////////////////////////////////////////
         // Tap Gesture Recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(gestureRecognize:)))
         view.addGestureRecognizer(tapGesture)
+
+ 
+//         Set up Vision Model
+                guard let selectedModel = try? VNCoreMLModel(for: CarRecognition().model) else { // (Optional) This can be replaced with other models on https://developer.apple.com/machine-learning/
+                    fatalError("Could not load model. Ensure model has been drag and dropped (copied) to XCode Project from https://developer.apple.com/machine-learning/ . Also ensure the model is part of a target (see: https://stackoverflow.com/questions/45884085/model-is-not-part-of-any-target-add-the-model-to-a-target-to-enable-generation ")
+                }
         
-        //////////////////////////////////////////////////
-        //Currently set to Canary Wharf
-        let pinCoordinate = CLLocationCoordinate2D(latitude: 33.7574, longitude: -84.4327)
-        let pinLocation = CLLocation(coordinate: pinCoordinate, altitude: 234)
-        let pinImage = UIImage(named: "arraw")!
-        let pinLocationNode = LocationAnnotationNode(location: pinLocation, image: pinImage)
-        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: pinLocationNode)
+                // Set up Vision-CoreML Request
+                let classificationRequest = VNCoreMLRequest(model: selectedModel, completionHandler: classificationCompleteHandler)
+                classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop // Crop from centre of images and scale to appropriate size.
+                visionRequests = [classificationRequest]
         
-        view.addSubview(sceneLocationView)
-        
-        // Set up Vision Model
-        //        guard let selectedModel = try? VNCoreMLModel(for: Inceptionv3().model) else { // (Optional) This can be replaced with other models on https://developer.apple.com/machine-learning/
-        //            fatalError("Could not load model. Ensure model has been drag and dropped (copied) to XCode Project from https://developer.apple.com/machine-learning/ . Also ensure the model is part of a target (see: https://stackoverflow.com/questions/45884085/model-is-not-part-of-any-target-add-the-model-to-a-target-to-enable-generation ")
-        //        }
-        //
-        //        // Set up Vision-CoreML Request
-        //        let classificationRequest = VNCoreMLRequest(model: selectedModel, completionHandler: classificationCompleteHandler)
-        //        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop // Crop from centre of images and scale to appropriate size.
-        //        visionRequests = [classificationRequest]
-        //
-        //        // Begin Loop to Update CoreML
-        //        loopCoreMLUpdate()
+                // Begin Loop to Update CoreML
+                loopCoreMLUpdate()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -79,25 +70,25 @@ class HomeViewController: UIViewController, ARSCNViewDelegate,  MKMapViewDelegat
         let configuration = ARWorldTrackingConfiguration()
         // Enable plane detection
         configuration.planeDetection = .horizontal
-        
+
         // Run the view's session
         sceneView.session.run(configuration)
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         // Pause the view's session
         sceneView.session.pause()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-    
+
     // MARK: - ARSCNViewDelegate
-    
+
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         DispatchQueue.main.async {
             // Do any desired updates to SceneKit here.
@@ -108,35 +99,35 @@ class HomeViewController: UIViewController, ARSCNViewDelegate,  MKMapViewDelegat
     override var prefersStatusBarHidden : Bool {
         return true
     }
-    
+
     // MARK: - Interaction
-    
+
     @objc func handleTap(gestureRecognize: UITapGestureRecognizer) {
         // HIT TEST : REAL WORLD
         // Get Screen Centre
         let screenCentre : CGPoint = CGPoint(x: self.sceneView.bounds.midX, y: self.sceneView.bounds.midY)
-        
+
         let arHitTestResults : [ARHitTestResult] = sceneView.hitTest(screenCentre, types: [.featurePoint]) // Alternatively, we could use '.existingPlaneUsingExtent' for more grounded hit-test-points.
-        
+
         if let closestResult = arHitTestResults.first {
             // Get Coordinates of HitTest
             let transform : matrix_float4x4 = closestResult.worldTransform
             let worldCoord : SCNVector3 = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
-            
+
             // Create 3D Text
             let node : SCNNode = createNewBubbleParentNode(latestPrediction)
             sceneView.scene.rootNode.addChildNode(node)
             node.position = worldCoord
         }
     }
-    
+
     func createNewBubbleParentNode(_ text : String) -> SCNNode {
         // Warning: Creating 3D Text is susceptible to crashing. To reduce chances of crashing; reduce number of polygons, letters, smoothness, etc.
         
         // TEXT BILLBOARD CONSTRAINT
         let billboardConstraint = SCNBillboardConstraint()
         billboardConstraint.freeAxes = SCNBillboardAxis.Y
-        
+
         // BUBBLE-TEXT
         let bubble = SCNText(string: text, extrusionDepth: CGFloat(bubbleDepth))
         var font = UIFont(name: "Futura", size: 0.15)
@@ -148,7 +139,7 @@ class HomeViewController: UIViewController, ARSCNViewDelegate,  MKMapViewDelegat
         bubble.firstMaterial?.isDoubleSided = true
         // bubble.flatness // setting this too low can cause crashes.
         bubble.chamferRadius = CGFloat(bubbleDepth)
-        
+
         // BUBBLE NODE
         let (minBound, maxBound) = bubble.boundingBox
         let bubbleNode = SCNNode(geometry: bubble)
@@ -167,25 +158,25 @@ class HomeViewController: UIViewController, ARSCNViewDelegate,  MKMapViewDelegat
         bubbleNodeParent.addChildNode(bubbleNode)
         bubbleNodeParent.addChildNode(sphereNode)
         bubbleNodeParent.constraints = [billboardConstraint]
-        
+
         return bubbleNodeParent
     }
-    
+
     // MARK: - CoreML Vision Handling
     
     func loopCoreMLUpdate() {
         // Continuously run CoreML whenever it's ready. (Preventing 'hiccups' in Frame Rate)
-        
+
         dispatchQueueML.async {
             // 1. Run Update.
             self.updateCoreML()
-            
+
             // 2. Loop this function.
             self.loopCoreMLUpdate()
         }
-        
+
     }
-    
+
     func classificationCompleteHandler(request: VNRequest, error: Error?) {
         // Catch Errors
         if error != nil {
@@ -196,24 +187,24 @@ class HomeViewController: UIViewController, ARSCNViewDelegate,  MKMapViewDelegat
             print("No results")
             return
         }
-        
+
         // Get Classifications
         let classifications = observations[0...1] // top 2 results
             .flatMap({ $0 as? VNClassificationObservation })
             .map({ "\($0.identifier) \(String(format:"- %.2f", $0.confidence))" })
             .joined(separator: "\n")
-        
-        
+
+
         DispatchQueue.main.async {
             // Print Classifications
             print(classifications)
             print("--")
-            
+
             // Display Debug Text on screen
             var debugText:String = ""
             debugText += classifications
             self.debugTextView.text = debugText
-            
+
             // Store the latest prediction
             var objectName:String = "…"
             objectName = classifications.components(separatedBy: "-")[0]
@@ -231,12 +222,12 @@ class HomeViewController: UIViewController, ARSCNViewDelegate,  MKMapViewDelegat
         let ciImage = CIImage(cvPixelBuffer: pixbuff!)
         // Note: Not entirely sure if the ciImage is being interpreted as RGB, but for now it works with the Inception model.
         // Note2: Also uncertain if the pixelBuffer should be rotated before handing off to Vision (VNImageRequestHandler) - regardless, for now, it still works well with the Inception model.
-        
+
         ///////////////////////////
         // Prepare CoreML/Vision Request
         let imageRequestHandler = VNImageRequestHandler(ciImage: ciImage, options: [:])
         // let imageRequestHandler = VNImageRequestHandler(cgImage: cgImage!, orientation: myOrientation, options: [:]) // Alternatively; we can convert the above to an RGB CGImage and use that. Also UIInterfaceOrientation can inform orientation values.
-        
+
         ///////////////////////////
         // Run Image Request
         do {
@@ -244,7 +235,7 @@ class HomeViewController: UIViewController, ARSCNViewDelegate,  MKMapViewDelegat
         } catch {
             print(error)
         }
-        
+
     }
 }
 
@@ -255,5 +246,6 @@ extension UIFont {
         return UIFont(descriptor: descriptor!, size: 0)
     }
 }
+
 
 
